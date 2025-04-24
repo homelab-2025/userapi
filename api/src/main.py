94759336562935b59386a5cd6b30c07e2c99dc28
Enzo G <gl@enzog.mozmail.com>
2025-04-24 14:05:56 +0200

    add main.py, dependencies with uv and compose to start a single node db

---
+++ api/src/main.py
@@ -0,0 +1,136 @@
+import os
+from typing import Optional, List
+
+from fastapi import FastAPI, Body, HTTPException, status
+from fastapi.responses import Response
+from pydantic import ConfigDict, BaseModel, Field
+from pydantic.functional_validators import BeforeValidator
+
+from typing_extensions import Annotated
+
+from bson import ObjectId
+import motor.motor_asyncio
+from pymongo import ReturnDocument
+
+
+app = FastAPI(
+    title="User Management API",
+    summary="API to manage a collection of users using FastAPI and MongoDB.",
+)
+client = motor.motor_asyncio.AsyncIOMotorClient(os.environ.get("MONGODB_URL","mongodb://localhost:27017"))
+db = client.usersdb
+user_collection = db.get_collection("users")
+
+PyObjectId = Annotated[str, BeforeValidator(str)]
+
+class UserModel(BaseModel):
+    id: Optional[PyObjectId] = Field(alias="_id", default=None)
+    username: str = Field(...)
+    firstname: str = Field(...)
+    lastname: str = Field(...)
+
+    model_config = ConfigDict(
+        populate_by_name=True,
+        arbitrary_types_allowed=True,
+        json_schema_extra={
+            "example": {
+                "username": "jdoe",
+                "firstname": "John",
+                "lastname": "Doe",
+            }
+        },
+    )
+
+
+class UpdateUserModel(BaseModel):
+    username: Optional[str] = None
+    firstname: Optional[str] = None
+    lastname: Optional[str] = None
+
+    model_config = ConfigDict(
+        arbitrary_types_allowed=True,
+        json_encoders={ObjectId: str},
+        json_schema_extra={
+            "example": {
+                "username": "jdoe",
+                "firstname": "John",
+                "lastname": "Doe",
+            }
+        },
+    )
+
+
+class UserCollection(BaseModel):
+    users: List[UserModel]
+
+
+@app.post(
+    "/users/",
+    response_description="Add new user",
+    response_model=UserModel,
+    status_code=status.HTTP_201_CREATED,
+    response_model_by_alias=False,
+)
+async def create_user(user: UserModel = Body(...)):
+    new_user = await user_collection.insert_one(
+        user.model_dump(by_alias=True, exclude=["id"])
+    )
+    created_user = await user_collection.find_one({"_id": new_user.inserted_id})
+    return created_user
+
+
+@app.get(
+    "/users/",
+    response_description="List all users",
+    response_model=UserCollection,
+    response_model_by_alias=False,
+)
+async def list_users():
+    return UserCollection(users=await user_collection.find().to_list(1000))
+
+
+@app.get(
+    "/users/{id}",
+    response_description="Get a single user",
+    response_model=UserModel,
+    response_model_by_alias=False,
+)
+async def show_user(id: str):
+    if (user := await user_collection.find_one({"_id": ObjectId(id)})) is not None:
+        return user
+    raise HTTPException(status_code=404, detail=f"User {id} not found")
+
+
+@app.put(
+    "/users/{id}",
+    response_description="Update a user",
+    response_model=UserModel,
+    response_model_by_alias=False,
+)
+async def update_user(id: str, user: UpdateUserModel = Body(...)):
+    user = {k: v for k, v in user.model_dump(by_alias=True).items() if v is not None}
+
+    if len(user) >= 1:
+        update_result = await user_collection.find_one_and_update(
+            {"_id": ObjectId(id)},
+            {"$set": user},
+            return_document=ReturnDocument.AFTER,
+        )
+        if update_result is not None:
+            return update_result
+        else:
+            raise HTTPException(status_code=404, detail=f"User {id} not found")
+
+    if (existing_user := await user_collection.find_one({"_id": id})) is not None:
+        return existing_user
+
+    raise HTTPException(status_code=404, detail=f"User {id} not found")
+
+
+@app.delete("/users/{id}", response_description="Delete a user")
+async def delete_user(id: str):
+    delete_result = await user_collection.delete_one({"_id": ObjectId(id)})
+    if delete_result.deleted_count == 1:
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+    raise HTTPException(status_code=404, detail=f"User {id} not found")
